82bd2c232386bf2987429b1ef9f9642ebb197a90
ElifDaglar <94562926+ElifDaglar@users.noreply.github.com>
2024-07-16 18:05:20 +0300

    Burc hesaplama

---
+++ cinBurcuHesaplama/src/MyList.java
@@ -0,0 +1,6 @@
+public class MyList<T> //type
+{
+    public void add(T value) {
+
+    }
+}
---
+++ cinBurcuHesaplama/src/generics.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class generics { //tip güvenli çalışmak için ihtiyaç duyarız.
+    public static void main(String[] args) {
+        ArrayList<String> sehirler =new ArrayList<>();
+        sehirler.add("Ankara");
+        sehirler.add("ist");
+
+        //customer
+        //product
+
+
+        //validator
+
+    }
+}
